3d0473a15c4fc65714008d8239d3d1c3379daa5c
ShabeenAbdulVaris <muhammadshabeen7@gmail.com>
2025-09-19 15:48:22 +0530

    modified fastapi app

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+# from twilio
 
 app = FastAPI()
 
 @app.get("/")
-async def read_root():
-    return {"Hello": "How are You"}
+async def read_root(request : Request):
+    query_params = dict(request.query_params)
+    headers = dict(request.headers)
+    client_host = request.client.host
+    return {
+        "method": request.method,
+        "url": str(request.url),
+        "client_ip": client_host,
+    }
 
+
+@app.post("/webhook")
+async def web_hook(name: str):
+    return f"Hello {name}"
